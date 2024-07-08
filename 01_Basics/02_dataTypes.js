9bd9087c06ce5774058515f8e7a4e9abe577464b
Abhishekbansod <bansodabhishek7@gmail.com>
2024-07-08 18:43:58 +0530

    dataTypes topic

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all the JS code as newer version
+//alert(3 + 3); // we are using node js & not browser. As this can be run on console.
+
+let name = "Abhishek" //String dataType
+let age = 23 //number dataType
+let isLoggedIn = true //boolean dataType
+
+/*PRIMITIVE DATA TYPES*/
+
+//numner => ranges from 2 to the power 53
+//bigint => used for very high numbers in stock market or facebook
+//string => can be used in "write_string" or 'write_string'
+//boolean => true/false
+//null => standalone value
+//undefined => when variable is not assigned with any value
+//symbol => used for uniqueness
+//object
+
+
+
+//Special dataTypes to remember
+console.table([typeof name, typeof age, typeof isLoggedIn]);
+
+console.log(typeof undefined); // output-> undefined
+console.log(typeof null); //output-> object
