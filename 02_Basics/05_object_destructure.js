5297918a8f80a871e171cc552db72de2cf08d413
Abhishekbansod <bansodabhishek7@gmail.com>
2024-07-25 11:19:03 +0530

    object destructuring & little bit about JSON & API's

---
+++ 02_Basics/05_object_destructure.js
@@ -0,0 +1,62 @@
+//how to destructure the object so that we don't need to access object elements again
+//and again with object name & object element name which you want to access
+
+//create an object
+
+const myObject = {
+    courseName: "JS with Projects",
+    coursePrice: "999",
+    courseTutor: "Abhishek Bansod",
+    courseValidity: "Lifetime Access"
+}
+
+//normal way to access object elements
+myObject.courseName
+myObject.courseValidity
+myObject.coursePrice
+
+//how to destructure the object & access myObject elements
+const { courseTutor, courseValidity, courseName, coursePrice } = myObject
+
+//display myObject elements -> no need to access it using object name like myObject.courseName
+console.log(courseName);
+console.log(coursePrice);
+
+//naming myObject elements while destructuring & displaying 
+const { courseTutor: tutor, courseValidity: validity } = myObject
+console.log(tutor);
+console.log(validity);
+
+
+//JSON -> Data comes in the form of JSON 
+//API's are used to fetch the data from database
+
+//how does JSON looks 
+{
+    "name" : "abhishek",
+    "id" : 1307755,
+    "company" : "Infosys",
+    "experience" : "18 Months"
+}
+
+//JSON -> in Array format as well
+[
+    {
+        "name": "abhishek",
+        "id": 1307755,
+        "company": "Infosys",
+        "experience": "18 Months"
+    },
+    {
+        "name": "abhishek",
+        "id": 1307755,
+        "company": "Infosys",
+        "experience": "18 Months"
+    },
+    {
+        "name": "abhishek",
+        "id": 1307755,
+        "company": "Infosys",
+        "experience": "18 Months"
+    }
+]
